4a52f119fedd8de905e34dcd0ece1d324b4e64c4
PlayZone <PlayZone@COMPUTER>
2022-06-10 17:08:36 +0300

    Добавьте файлы проекта.

---
+++ Torikov/Data/TorikovContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Torikov.Models;
+
+namespace Torikov.Data
+{
+    public class TorikovContext : DbContext
+    {
+        public TorikovContext (DbContextOptions<TorikovContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Torikov.Models.Movie> Movie { get; set; }
+    }
+}
